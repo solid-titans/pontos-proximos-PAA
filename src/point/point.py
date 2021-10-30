b881cebf5aadda018c1aa34c28ad86efab3bcfde
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-10-30 16:26:24 -0300

    feat: transalating .cpp to .py

--- src/point/point.py
+++ src/point/point.py
@@ -3,3 +3,14 @@ class Point():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+
+    def __str__(self):
+        return f"X = {self.x}\nY = {self.y}\n"
+
+
+    def get_x(self):
+        return self.x
+    
+    def get_y(self):
+        return self.y
